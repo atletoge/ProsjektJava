ffa5411370cc6cffa67e5d578df8a9f54da89c23
Atle Tøge <atle@dhcp-10-22-15-131.wlan.ntnu.no>
2022-03-02 14:05:21 +0100

    startconfig av personklasse

--- src/main/java/karakter/person.java
+++ src/main/java/karakter/person.java
@@ -1,5 +1,37 @@
 package karakter;
+import java.util.regex.Pattern;
 
-public class person {
+
+public class Person {
     
+
+    private String name;
+    private int studentID;
+    private String email;
+
+    public Person(String name, int studentID, String email) {
+        validateName(name);
+        this.name = name;
+    }
+
+    private boolean validateName(String name) {
+        if(name.matches("^([a-zA-ZæøåÆØÅ]{2,}\\s[a-zA-ZæøåÆØÅ]{1,}'?-?[a-zA-ZæøåÆØÅ]{2,}\\s?([a-zA-ZæøåÆØÅ]{1,})?)")) {
+           return true; 
+        } 
+        else {
+            throw new IllegalArgumentException("Navnet du oppga er ikke gyldig");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    
+
+
+    public static void main(String[] args) {
+        Person person = new Person("Atle Østrem Tøge", 111002, "atletoge@gmail.com");
+    }
+
 }
